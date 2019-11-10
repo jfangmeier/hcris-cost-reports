8152b3378595d0d59bd4f6a94bfbed24b11a8f01
Josh Fangmeier <jfangmeier@gmail.com>
2019-11-10 16:14:59 -0600

    moved files

--- archive/hospital-plot.R
+++
@@ -1,7 +0,0 @@
-cost_reports %>% 
-  filter(state == "MN" & year %in% 2012:2017) %>% 
-  group_by(year) %>% 
-  summarize(tot_ftemp = sum(ftemp)) %>% 
-  ggplot(aes(x = year, y = tot_ftemp)) +
-  geom_bar(stat = "sum")
-  
